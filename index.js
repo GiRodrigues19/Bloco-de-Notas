1690f3ee0e2c6237de3520a5c2bde9d22d49f9f7
GiRodrigues19 <giovanna19_98@live.com>
2025-05-27 19:45:10 -0300

    hello

---
+++ index.js
@@ -0,0 +1,20 @@
+let nomeUsuario = document.getElementById("nome").value;
+let anoNascimento = Number(document.getElementById("nascimento").value);
+let anoAtual = Number(document.getElementById("anoAtual").value);
+
+
+
+anoAtual = Number (anoAtual);
+idadeUsuario = Number (idadeUsuario);
+
+
+alert ("Voce tem " + anoNascimento + " anos ")
+if (anoNascimento <= 18){
+  alert ("Você nao tem idade para acessar" + nomeUsuario + "!")
+}else{
+  alert ("Você tem idade para prosseguir, " + nomeUsuario + ".")
+}
+
+
+
+
